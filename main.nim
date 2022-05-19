c1ba2858da28e48f9f03b552d3d9bb95c080009c
yelsharawy <yelsharawy20@stuy.edu>
2022-05-19 15:57:27 -0400

    Completed pre-processing step

--- main.nim
+++ main.nim
@@ -1,12 +1,22 @@
 #!/bin/env -S nim r
-import os
+import std/[os, strutils]
 
-func preprocess(data : var seq[byte]) =
+# "toString" for viewing data in binary
+proc `$`(s : seq[byte]) : string =
+    const bytesPerLine = 8
+    for i in countup(0, s.len-7, bytesPerLine):
+        for j in i..<min(s.len, i+8):
+            result &= (int s[j]).toBin(8)
+            result &= " "
+        result[^1] = '\n' # replace last space with newline
+    # result.setLen result.len-1 # trim off last newline
+
+proc preprocess(data : var seq[byte]) =
     let originalLength = data.len
     # add single 1 bit
     data &= 0b1000_0000
     # set length to closest multiple of 512 bits (64 bytes), minus 8
-    data.setLen (((data.len+8) div 64)+1) * 64 - 8
+    data.setLen (((data.len+7) div 64)+1) * 64 - 8
     for i in countdown(7, 0):
         data &= byte(originalLength shr (i*8))
     assert data.len mod 64 == 0
