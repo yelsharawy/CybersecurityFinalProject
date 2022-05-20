ee6d3ebd72ec7e22a8faef0a5b2a37863b6da347
Angela Cai <angelacai@Acais-Mac.T-mobile.com>
2022-05-19 22:54:54 -0400

    tried to right rotate bits

---
+++ cyber_final_project.py
@@ -0,0 +1,18 @@
+w = "01101111001000000111011101101111"
+
+# note that this works for shifts lesss thann the length
+def rotate(w, shift):
+    first = w[0:len(w)-shift]
+    second = w[len(w)-shift:]
+    return second + first
+
+def xor(w1, w2):
+    ans = ""
+    for i in range(len(w1)):
+        if w1[i] == w2[i]:
+            ans += "0"
+        else:
+            ans += "1";
+    return ans
+s0 = xor(xor(rotate(w, 7),rotate(w, 18)), rotate(w, 3))
+s1 = xor(xor(rotate(w, 17),rotate(w, 19)), rotate(w, 10))
