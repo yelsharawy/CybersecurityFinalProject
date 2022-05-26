3ebd997a44592496bf82f8a9d926a32d0740a7c5
yelsharawy <yelsharawy20@stuy.edu>
2022-05-26 15:36:53 -0400

    Worked towards generalizing hash for all forms of SHA

--- main.nim
+++ main.nim
@@ -1,13 +1,24 @@
 #!/bin/env -S nim r
-import std/[os, strutils, endians, sugar]
+import std/[os, strutils, endians, tables, sugar]
 
-type Hash = array[8, uint32]
+when not defined(debug):
+    template dump(x : untyped) = discard
+
+type
+    HashConfig {.byref.} = object
+        numIterations : int
+        chunkSize : int
+        initialHash : Hash
+        hashLength : int # crops length of output to this many words
+        roundConstants : ptr UncheckedArray[uint32] # should have same length as `numIterations`
+    Hash = seq[uint32]
 
 proc `$`(a : Hash) : string =
     for x in a:
         result &= x.toHex 8
+    result = result.toLowerAscii
 
-const roundConstants : array[64, uint32] = [
+const sha256roundConstants : array[64, uint32] = [
     0x428a2f98'u32, 0x71374491'u32, 0xb5c0fbcf'u32, 0xe9b5dba5'u32, 0x3956c25b'u32, 0x59f111f1'u32, 0x923f82a4'u32, 0xab1c5ed5'u32,
     0xd807aa98'u32, 0x12835b01'u32, 0x243185be'u32, 0x550c7dc3'u32, 0x72be5d74'u32, 0x80deb1fe'u32, 0x9bdc06a7'u32, 0xc19bf174'u32,
     0xe49b69c1'u32, 0xefbe4786'u32, 0x0fc19dc6'u32, 0x240ca1cc'u32, 0x2de92c6f'u32, 0x4a7484aa'u32, 0x5cb0a9dc'u32, 0x76f988da'u32,
@@ -17,6 +28,63 @@ const roundConstants : array[64, uint32] = [
     0x19a4c116'u32, 0x1e376c08'u32, 0x2748774c'u32, 0x34b0bcb5'u32, 0x391c0cb3'u32, 0x4ed8aa4a'u32, 0x5b9cca4f'u32, 0x682e6ff3'u32,
     0x748f82ee'u32, 0x78a5636f'u32, 0x84c87814'u32, 0x8cc70208'u32, 0x90befffa'u32, 0xa4506ceb'u32, 0xbef9a3f7'u32, 0xc67178f2'u32
 ]
+
+# const sha512roundConstants : array[80, uint64] = [
+#     0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc, 0x3956c25bf348b538, 
+#     0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118, 0xd807aa98a3030242, 0x12835b0145706fbe, 
+#     0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2, 0x72be5d74f27b896f, 0x80deb1fe3b1696b1, 0x9bdc06a725c71235, 
+#     0xc19bf174cf692694, 0xe49b69c19ef14ad2, 0xefbe4786384f25e3, 0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65, 
+#     0x2de92c6f592b0275, 0x4a7484aa6ea6e483, 0x5cb0a9dcbd41fbd4, 0x76f988da831153b5, 0x983e5152ee66dfab, 
+#     0xa831c66d2db43210, 0xb00327c898fb213f, 0xbf597fc7beef0ee4, 0xc6e00bf33da88fc2, 0xd5a79147930aa725, 
+#     0x06ca6351e003826f, 0x142929670a0e6e70, 0x27b70a8546d22ffc, 0x2e1b21385c26c926, 0x4d2c6dfc5ac42aed, 
+#     0x53380d139d95b3df, 0x650a73548baf63de, 0x766a0abb3c77b2a8, 0x81c2c92e47edaee6, 0x92722c851482353b, 
+#     0xa2bfe8a14cf10364, 0xa81a664bbc423001, 0xc24b8b70d0f89791, 0xc76c51a30654be30, 0xd192e819d6ef5218, 
+#     0xd69906245565a910, 0xf40e35855771202a, 0x106aa07032bbd1b8, 0x19a4c116b8d2d0c8, 0x1e376c085141ab53, 
+#     0x2748774cdf8eeb99, 0x34b0bcb5e19b48a8, 0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb, 0x5b9cca4f7763e373, 
+#     0x682e6ff3d6b2b8a3, 0x748f82ee5defb2fc, 0x78a5636f43172f60, 0x84c87814a1f0ab72, 0x8cc702081a6439ec, 
+#     0x90befffa23631e28, 0xa4506cebde82bde9, 0xbef9a3f7b2c67915, 0xc67178f2e372532b, 0xca273eceea26619c, 
+#     0xd186b8c721c0c207, 0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178, 0x06f067aa72176fba, 0x0a637dc5a2c898a6, 
+#     0x113f9804bef90dae, 0x1b710b35131c471b, 0x28db77f523047d84, 0x32caab7b40c72493, 0x3c9ebe0a15c9bebc, 
+#     0x431d67c49c100d4c, 0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817
+# ]
+
+converter uncheck[N;T](arr : array[N,T]) : auto =
+    result = cast[ptr UncheckedArray[T]](unsafeAddr arr[0])
+
+var builtinConfigs = toTable[string, HashConfig]({
+    "sha256": HashConfig(
+        numIterations: 64,
+        chunkSize: 64,
+        initialHash: @[
+            0x6a09e667'u32,
+            0xbb67ae85'u32,
+            0x3c6ef372'u32,
+            0xa54ff53a'u32,
+            0x510e527f'u32,
+            0x9b05688c'u32,
+            0x1f83d9ab'u32,
+            0x5be0cd19'u32
+        ],
+        hashLength: 8,
+        roundConstants: uncheck sha256roundConstants,
+    ),
+    "sha224": HashConfig(
+        numIterations: 64,
+        chunkSize: 64,
+        initialHash: @[
+            0xc1059ed8'u32,
+            0x367cd507'u32,
+            0x3070dd17'u32,
+            0xf70e5939'u32,
+            0xffc00b31'u32,
+            0x68581511'u32,
+            0x64f98fa7'u32,
+            0xbefa4fa4'u32
+        ],
+        hashLength: 7,
+        roundConstants: uncheck sha256roundConstants,
+    )
+})
 
 # "toString" for viewing data in binary
 proc `$`[T : SomeInteger](s : seq[T]) : string =
@@ -86,7 +154,7 @@ proc addCompressed(hash : var Hash; w : openArray[uint32]) =
         let
             S1 = rightRotate(hashVars[4], 6) xor rightRotate(hashVars[4], 11) xor rightRotate(hashVars[4], 25)
             ch = (hashVars[4] and hashVars[5]) xor ((not hashVars[4]) and hashVars[6])
-            temp1 = hashVars[7] + S1 + ch + roundConstants[i] + w[i]
+            temp1 = hashVars[7] + S1 + ch# + roundConstants[i] + w[i]
             S0 = rightRotate(hashVars[0], 2) xor rightRotate(hashVars[0], 13) xor rightRotate(hashVars[0], 22)
             maj = (hashVars[0] and hashVars[1]) xor (hashVars[0] and hashVars[2]) xor (hashVars[1] and hashVars[2])
             temp2 = S0 + maj
@@ -113,16 +181,8 @@ proc sha256(data : seq[byte]) : Hash =
     dump padded
     dump padded.len
     # Step 2: Initialize hash values
-    result = [
-        0x6a09e667'u32,
-        0xbb67ae85'u32,
-        0x3c6ef372'u32,
-        0xa54ff53a'u32,
-        0x510e527f'u32,
-        0x9b05688c'u32,
-        0x1f83d9ab'u32,
-        0x5be0cd19'u32
-    ]
+    # result = 
+    # result = 
     # Step 4: Chunk loop
     
     for chunk in padded.chunks:
@@ -143,5 +203,5 @@ when isMainModule:
     var # read from first argument, or stdin if none given
         inputStr = if paramCount() >= 1: readFile(paramStr(1)) else: stdin.readAll
     
-    dump sha256(inputStr)
+    echo sha256(inputStr)
     
